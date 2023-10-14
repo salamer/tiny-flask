6916647926060c1341dc9be2c6c605f8902f761b
salamer <salamer_gaga@163.com>
2023-10-14 16:04:02 +0800

    add: add file watcher

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def hello_world():
     print("hello")
     l = []
-    base = "*" * 1024
+    base = "*" * 1024 * 1024
     for i in range(0, 1024*1024):
         l.append(base)
     print("mem", sys.getsizeof(l))
-    return "<p>Hello, Aljun!</p>" + sys.getsizeof(l).__str__
+    return "<p>Hello, Aljun!</p> {}".format(sys.getsizeof(l))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=False)
