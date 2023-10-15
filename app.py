549e80d0ff086c1c602d0d8199b12d9e5214abe1
salamer <salamer_gaga@163.com>
2023-10-15 19:28:56 +0800

    update: update ui

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 def hello_world():
     print("hello")
     l = []
+    x = bytearray(1024*1024*1000)
+
     # base = bytearray(
     #     1024 * 1024 * 512
     # )
     # print(sys.getsizeof(base))
     # return "<p>Hello, Aljun!</p> {}".format(sys.getsizeof(base))
-    return "<p>Hello, Aljun!</p>"
+    return "<p>Hello, Aljun!</p> {}".format(x)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=False)
