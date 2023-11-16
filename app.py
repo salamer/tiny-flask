d3d2d50d6de7c1da25ab689a4c4c1bc6c06e19f3
salamer <salamer_gaga@163.com>
2023-11-17 01:14:01 +0800

    update: update ui

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 import sys
 import datetime
 
@@ -73,6 +73,11 @@ def cookie2():
         expires=datetime.date.today() + datetime.timedelta(days=7)
     )
     return resp
+
+@app.route("/headers")
+def headers():
+    print(request.headers)
+    return {"headers": dict(request.headers)}
 
 
 if __name__ == "__main__":
